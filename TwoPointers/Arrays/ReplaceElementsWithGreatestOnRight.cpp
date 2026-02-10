9dc0c0fc140d82b0795d42406576d1b9110f3612
guptaarushi2312-a11y <gupta.arushi2312@gmail.com>
2026-02-11 01:40:47 +0530

    Create ReplaceElementsWithGreatestOnRight.cpp

---
+++ TwoPointers/Arrays/ReplaceElementsWithGreatestOnRight.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+       int maxr = -1;
+       for(int i=arr.size()-1;i>=0;i--)
+       {
+          int curr = arr[i];
+          arr[i] = maxr;
+          maxr = max(maxr,curr);
+       }
+       return arr;
+        
+        
+    }
+};
